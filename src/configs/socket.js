ec2f058b47d7c4b1910608cffa7584fa2e77fdab
LongNH <longnh@phenikaa-x.com>
2023-11-20 18:18:01 +0700

    Fix socket

--- src/configs/socket.js
+++ src/configs/socket.js
@@ -1,6 +1,10 @@
 import { io } from "socket.io-client";
 
 const url = process.env.NEXT_PUBLIC_WS_URL;
-const socket = io(url);
+const options = {
+   transports: ["websocket"],
+   withCredentials: true,
+}
+const socket = io(url, options);
 
 export default socket;
