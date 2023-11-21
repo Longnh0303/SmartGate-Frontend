de1be908c8afe00c0490079815915aedcb44695a
Nguyen Hai Long <nguyenhailongtlu@gmail.com>
2023-11-21 14:31:23 +0700

    fix socket option

--- src/configs/socket.js
+++ src/configs/socket.js
@@ -3,6 +3,7 @@ import { io } from "socket.io-client";
 const url = process.env.NEXT_PUBLIC_WS_URL;
 const options = {
   transports: ["websocket"],
+  withCredentials: true,
 };
 const socket = io(url, options);
 
