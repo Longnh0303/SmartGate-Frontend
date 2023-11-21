c1738080e0ccd6b372501a8e42bc9bb075544128
Nguyen Hai Long <nguyenhailongtlu@gmail.com>
2023-11-21 16:58:05 +0700

    fix socket n lan

--- src/configs/socket.js
+++ src/configs/socket.js
@@ -2,7 +2,7 @@ import { io } from "socket.io-client";
 
 const url = process.env.NEXT_PUBLIC_WS_URL;
 const options = {
-  transports: ["websocket"], // Chỉ sử dụng WebSocket
+  transports: ["polling", "websocket"], // Chỉ sử dụng WebSocket
   withCredentials: true,
 };
 const socket = io(url, options);
