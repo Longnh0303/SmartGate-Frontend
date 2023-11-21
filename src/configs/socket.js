90aa7f812b67794d413c5c97b16211120123f5cc
Nguyen Hai Long <nguyenhailongtlu@gmail.com>
2023-11-21 14:57:33 +0700

    fix socket pr pn

--- src/configs/socket.js
+++ src/configs/socket.js
@@ -2,6 +2,7 @@ import { io } from "socket.io-client";
 
 const url = process.env.NEXT_PUBLIC_WS_URL;
 const options = {
+  transports: ["websocket"], // Chỉ sử dụng WebSocket
   withCredentials: true,
 };
 const socket = io(url, options);
